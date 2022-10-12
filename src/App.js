fbef0fabc080d7ccdddc51cf20738bfaf08e274d
Muhammed Ali <muhammedaliaydin97@gmail.com>
2022-10-12 23:22:46 +0300

    Header initialized

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from "react";
 
 // import components
-import Hero from './components/Hero';
-import Header from './components/Header';
-import About from './components/About';
-import GallerySection from './components/GallerySection';
-import Skills from './components/Skills';
-import Interview from './components/Interview';
-import Testimonial from './components/Testimonial';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-import Copyright from './components/Copyright';
+import Hero from "./components/Hero";
+import Header from "./components/Header";
+import About from "./components/About";
+import GallerySection from "./components/GallerySection";
+import Skills from "./components/Skills";
+import Interview from "./components/Interview";
+import Testimonial from "./components/Testimonial";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Copyright from "./components/Copyright";
 
 const App = () => {
   return (
-    <div className='max-w-[1920px] mx-auto overflow-hidden bg-white'>
+    <div className="max-w-[1920px] mx-auto overflow-hidden bg-white">
       <Header />
       <Hero />
       <About />
@@ -25,7 +25,7 @@ const App = () => {
       <Contact />
       <Footer />
       <Copyright />
-      {/* <div className='h-[4000px]'></div> */}
+      <div className="h-[4000px]"></div>
     </div>
   );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { headerData } from "../data";
+import Nav from "./Nav";
+import NavMobile from "./NavMobile";
+import Socials from "./Socials";
+import { TiThMenuOutline } from "react-icons/ti";
 
 const Header = () => {
-  return <div>Header</div>;
+  const { logo } = headerData;
+  const [isActive, setIsActive] = useState(false);
+  const [navMobile, setNavMobile] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setIsActive(true) : setIsActive(false);
+    });
+  });
+  return (
+    <header
+      className={`${isActive ? "h-[100px] lg:h-[110px] shadow-lg" : "h-[120px] lg:h-[150px]"}
+    fixed left-0 right-0 top-0 z-10 max-w-[1920px] w-full mx-auto transistion-all duration-300`}
+    >
+      <div className="flex justify-between items-center h-full pl-[50px] pr-[60px]">
+        <a href="/">
+          <img className="w-[188x] h-[90px]" src={logo} alt="" />
+        </a>
+
+        <div className="hidden xl:flex">
+          {/* Desktop */}
+          <Nav />
+        </div>
+
+        <div className="xl:hidden absoule right-[5%] bg-dark text-white p-2 rounded-md cursor-pointer">
+          {/* Mobile Menu Icon*/}
+          <TiThMenuOutline className="text-3xl" />
+        </div>
+
+        <div className="fixed bg-red-500 w-full h-full left-0 -z-10 transistion-all duration-300">
+          <NavMobile />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
