4765a45f14c190afbefb1430b9cd43209229032c
Muhammed Ali <muhammedaliaydin97@gmail.com>
2022-10-13 08:31:02 +0300

    Gallery section implemented

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import React, { useState } from "react";
+import { galleryData } from "../data";
+import { PhotoAlbum } from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
+const slides = galleryData.images.map(({ original, width, height }) => ({
+  src: original,
+  width,
+  height,
+}));
 
 const GallerySection = () => {
-  return <div>GallerySection</div>;
+  const [index, setIndex] = useState(-1);
+  const { title, btnText, btnIcon, images } = galleryData;
+
+  return (
+    <section className="bg-[#f9f9f9] section relative mt-[40p] lg:mt-0">
+      <div className="container mx-auto">
+        <motion.h2
+          className="h2 max-w-[370px] lg:mb-20"
+          variants={fadeIn("up")}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: false, amount: 0.6 }}
+        >
+          {title}
+        </motion.h2>
+      </div>
+      <motion.div
+        className="mb-8 lg:mb-20"
+        variants={fadeIn("up")}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.2 }}
+      >
+        <PhotoAlbum onClick={(event, photo, index) => setIndex(index)} layout="rows" photos={images} />
+        <Lightbox
+          slides={slides}
+          styles={{ container: { backgroundColor: "rgba(0,0,0, .9)" } }}
+          open={index >= 0}
+          index={index}
+          close={() => setIndex(-1)}
+        ></Lightbox>
+      </motion.div>
+      <motion.div
+        className="flex justify-center"
+        variants={fadeIn("up")}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.2 }}
+      >
+        <button className="btn btn-lg btn-dark">
+          {btnText} <div className="text-xl">{btnIcon}</div>
+        </button>
+      </motion.div>
+    </section>
+  );
 };
 
 export default GallerySection;
